d462f2cee43c1dfd6180b4889c71a3327fb9ace3
Sejal Gupta <sejalg@mit.edu>
2021-09-18 13:52:14 -0400

    preferences.py

--- preferences.py
+++ preferences.py
@@ -1,13 +1,8 @@
 import datetime 
 
-time_blocks = {
-        'a' : (datetime.time(0,0,0), datetime.time(3,59,59)),
-        'b' : (datetime.time(4,0,0), datetime.time(7,59,59)),
-        'c' : (datetime.time(8,0,0), datetime.time(11,59,59)),
-        'd' : (datetime.time(12,0,0), datetime.time(15,59,59)),
-        'e' : (datetime.time(16,0,0), datetime.time(19,59,59)),
-        'f' : (datetime.time(20,0,0), datetime.time(23,59,59))
-    }
+time_blocks = []
+for num in range(24):
+    time_blocks.append((datetime.time(num,0,0), datetime.time(num,59,59))) 
 
 class Preferences:
 
@@ -15,12 +10,18 @@ class Preferences:
         self.sleep_start = sleep_start
         self.end_start = sleep_end
 
-        #list of preferences for energy by 4 hour time blocks
+        #list of preferences for energy by early morning (4 - 8), late morning (8 - 12), afternoon (12 - 4), early evening (4 - 8), late evening (8 - 12), night (12 - 4)
         #on a scale from 0 to 5
-        self.energy_preferences = energy_preferences
+        energies = []
+        for pref in energy_preferences:
+            energies.append(pref)
+            energies.append(pref)
+            energies.append(pref)
+            energies.append(pref)
+        self.energy_preferences = energies
         
-        for i, block in enumerate(time_blocks):
-            if (sleep_start <= time_blocks[block][1]) and  (sleep_end >= time_blocks[block][0]):
+        for i in range(len(time_blocks)):
+            if (sleep_start <= time_blocks[i][1]) and  (sleep_end >= time_blocks[i][0]):
                 self.energy_preferences[i] = 0
 
     def get_energy_pref(self):
