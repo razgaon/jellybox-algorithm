7e62b2f9c1bdf5239d4626341ec3db6b6c780c73
Radha Patel <rrpatel@mit.edu>
2021-09-18 13:52:17 -0400

    Implement task scheduling

--- schedule.py
+++ schedule.py
@@ -1,29 +1,95 @@
 HOURS_PER_DAY = 24
+MINUTES_PER_HOUR = 60
 BLOCKS_PER_HOUR = 4
 
+import datetime
 class Schedule:
-    def __init__(self):
-        self.weeks = []
+    def __init__(self, start_date, end_date):
+        self.days = self.generate_blank_schedule(start_date, end_date)
 
-    def add_task(self):
-        pass
+    def generate_blank_schedule(self, start_date, end_date):
+        delta = datetime.timedelta(days = 1)
+        days = {}
+        while start_date <= end_date:
+            days[start_date] = Day()
+            start_date += delta
+        return days
 
-class Week(Schedule):
-    def __init__(self):
-        self.days = []
+    def add_task(self, task):
+        for day, day_schedule in self.days.items():
+            if day < task.start_date:
+                continue
+            if day > task.due_date:
+                break
+            is_time_available, start_time = day_schedule.find_time_for_task(task)
 
-class Day(Week):
+            if is_time_available:
+                hour, block = start_time
+                day_schedule.schedule_task(task, hour, block)
+                return
+
+    def display_schedule(self):
+        for day, day_schedule in self.days.items():
+            print(day_schedule.times)
+
+class Day:
     def __init__(self):
-        self.ordered_times = [0] * HOURS_PER_DAY * BLOCKS_PER_HOUR
+        self.times = [[None] * BLOCKS_PER_HOUR for _ in range(HOURS_PER_DAY)]
+
+    def get_open_times(self):
+        open_times = [set() for _ in range(HOURS_PER_DAY)]
+        for i in range(HOURS_PER_DAY):
+            for j in range(BLOCKS_PER_HOUR):
+                if not self.times[i][j]:
+                    open_times[i].add(j)
+        return open_times
+
+    def find_time_for_task(self, task):
+        start_time = (0, 0)
+        curr_duration = 0
+        open_times = self.get_open_times()
+        restart_time = False
+        for i, hour in enumerate(open_times):
+            for block in [0, 1, 2, 3]:
+                if curr_duration >= task.duration:
+                    return True, start_time
+                if block in hour:
+                    if restart_time:
+                        start_time = (i, block)
+                        restart_time = False
+                    curr_duration += 15
+                else:
+                    curr_duration = 0
+                    restart_time = True
+        return False, (0, 0)
+
+    def schedule_task(self, task, hour, block):
+        minutes_per_block = MINUTES_PER_HOUR / BLOCKS_PER_HOUR
+        for i in range(int(task.duration // minutes_per_block)):
+            print(hour, block)
+            self.times[hour][block] = task
+            if block >= BLOCKS_PER_HOUR - 1:
+                hour += 1
+                block = 0
+            else:
+                block += 1
 
 class Task:
-    def __init__(self, priority, start_date, difficulty, due_date):
+    def __init__(self, priority, difficulty, start_date, due_date, duration):
         self.priority = priority
         self.difficulty = difficulty
         self.start_date = start_date
         self.due_date = due_date
+        self.duration = duration
 
 
 def add_task_to_schedule(task, schedule):
     pass
         
+
+start_date = datetime.date(2020, 1, 1)
+end_date = datetime.date(2020, 1, 4)
+test_schedule = Schedule(start_date, end_date)
+test_task = Task(1, 1, datetime.date(2020, 1, 2), datetime.date(2020, 1, 3), 75)
+test_schedule.add_task(test_task)
+test_schedule.display_schedule()
